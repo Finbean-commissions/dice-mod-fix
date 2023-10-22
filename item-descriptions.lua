374c9e9c77b0747f72bf1c9926119934ca8682b3
Finbean's <finbean.commissions@gmail.com>
2023-10-22 10:02:31 +0100

    crooked-item

--- item-descriptions.lua
+++ item-descriptions.lua
@@ -8,11 +8,13 @@ mod.item = {
 
 mod.description = {
 	GoldenItem = "Upon use, rerolls every pedestal item in the room into a quality 3 or 4 GoldenItem item, but gives the player 2 broken hearts#{{Warning}} May freeze or crash the game if there are alot of item pedestals in the room",
+	CrookedItem = "when used, has a 50% chance to reroll every pedestal item in the room and duplicate them, but has a 50% chance to remove every item in the room and spawn a Dice Shard#{{Warning}} May freeze or crash the game if there are alot of item pedestals in the room"
 }
 
 --External Item Descriptions documentation found here: https://github.com/wofsauge/External-Item-Descriptions/wiki.
 if EID then
 	EID:addCollectible(mod.item.GoldenItem, mod.description.GoldenItem)
+	EID:addCollectible(mod.item.CrookedItem, mod.description.CrookedItem)
 end
 
 --Encyclopedia documentation found here: https://github.com/AgentCucco/encyclopedia-docs/wiki.
@@ -25,10 +27,25 @@ if Encyclopedia then
 				{str = "Warning! May freeze or crash the game if there are alot of item pedestals in the room."},
 			},
 		},
+		CrookedItem = {
+			{ -- Description
+				{str = "description", fsize = 2, clr = 3, halign = 0},
+				{str = "when used, has a 50% chance to reroll every pedestal item in the room and duplicate them, but has a 50% chance to remove every item in the room and spawn a Dice Shard."},
+				{str = "Warning! May freeze or crash the game if there are alot of item pedestals in the room."},
+			},
+		},
 	}
 	Encyclopedia.AddItem({
 		ID = mod.item.GoldenItem,
 		WikiDesc = Wiki.GoldenItem,
+		Pools = {
+			Encyclopedia.ItemPools.POOL_TREASURE,
+			Encyclopedia.ItemPools.POOL_GREED_TREASURE,
+		},
+	})
+	Encyclopedia.AddItem({
+		ID = mod.item.CrookedItem,
+		WikiDesc = Wiki.CrookedItem,
 		Pools = {
 			Encyclopedia.ItemPools.POOL_TREASURE,
 			Encyclopedia.ItemPools.POOL_GREED_TREASURE,
--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@
 local mod = RegisterMod("Too many D6s! (Fix)", 1)
 local game = Game()
 local rng = RNG()
-local seed = game:GetSeeds():GetStartSeed()
 
 mod.Items = {
     GoldenItem = Isaac.GetItemIdByName("Golden D6"),
@@ -18,13 +17,13 @@ function mod:UseItem(item, _, player, UseFlags, Slot, _)
 			for i, entity in ipairs(Isaac.GetRoomEntities()) do
 				if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE then
 
-					player:UseItem(CollectibleType.COLLECTIBLE_D6, 0, -1, 0)
+					player:UseActiveItem(CollectibleType.COLLECTIBLE_D6, 0, -1, 0)
 					item = Isaac.GetItemConfig():GetCollectible(entity.SubType)
 
 					player:AddBrokenHearts(2)
 
 					while item.Quality < 3 do
-						player:UseItem(CollectibleType.COLLECTIBLE_D6, UseFlag.USE_NOANIM, -1, 0)
+						player:UseActiveItem(CollectibleType.COLLECTIBLE_D6, UseFlag.USE_NOANIM, -1, 0)
 						player:AnimateCollectible(mod.Items.GoldenItem, "UseItem")
 						item = Isaac.GetItemConfig():GetCollectible(entity.SubType)
 					end
@@ -32,18 +31,29 @@ function mod:UseItem(item, _, player, UseFlags, Slot, _)
 			end
         end
 		--Crooked D6
-		if item == mod.Items.GoldenItem then
-			player:AnimateCollectible(mod.Items.GoldenItem, "UseItem")
+		if item == mod.Items.CrookedItem then
+			player:AnimateCollectible(mod.Items.CrookedItem, "UseItem")
 
+			local seed = game:GetSeeds():GetStartSeed()
 			rng:SetSeed(seed, 35)
-        	local roll = rng:RandomFloat(1)
+        	local roll = rng:RandomInt(2)
+			print(roll)
 
 			if roll == 0 then
 				for i, entity in ipairs(Isaac.GetRoomEntities()) do
 					if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE then
-
+						player:UseActiveItem(CollectibleType.COLLECTIBLE_D6, 0, -1, 0)
+						player:UseActiveItem(CollectibleType.COLLECTIBLE_DIPLOPIA, 0, -1, 0)
 					end
 				end
+			elseif roll == 1 then
+				for i, entity in ipairs(Isaac.GetRoomEntities()) do
+					if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+						Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, entity.Position, Vector(0,0), nil)
+						entity:Remove()
+					end
+				end
+				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TAROTCARD, Card.CARD_DICE_SHARD, player.Position, Vector(0,0), nil)
 			end
         end
     end
